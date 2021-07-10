53b178929bd8f64f9d98e111d000e68c90309afa
jumpei <jumpei429@gmail.com>
2021-07-10 23:46:41 +0900

    ログイン機能実装

--- Views/sign-in.php
+++ Views/sign-in.php
@@ -1,9 +1,4 @@
-<?php
-// 設定関連を読み込む。includeは他のファイルの記述を取り込むときに使う。include_onceは同じファイルは2回includeされていても1回しか実行しない。
-include_once('../config.php'); 
-// 便利な関数を読み込む
-include_once('../util.php');
-?>
+
 
 <!DOCTYPE html>
 <html lang="ja">
@@ -20,6 +15,14 @@ include_once('../util.php');
         <form action="sign-in.php" method="post">
             <img src="/TwitterClone/Views/img/logo-white.svg" alt="" class="logo-white">
             <h1>Twitterクローンにログイン</h1>
+
+            <?php if (isset($view_try_login_result) && $view_try_login_result === false) : ?>
+                <!-- ログインに失敗した場合 -->
+                <div class="alert alert-warning text-sm" role="alert">
+                    ログインに失敗しました。メールアドレス、パスワードが正しいかご確認下さい。
+                </div>
+            <?php endif; ?>
+
             <input type="email" class="form-control" name="email" placeholder="メールアドレス" required autofocus>
             <input type="password" class="form-control" name="password" placeholder="パスワード" required>
             <button class="w-100 btn btn-lg" type="submit">ログイン</button>
